14efd265c7a0d087c57b0e3bb3f35f3996a4a8e9
Michael Florya <mih2303@mail.ru>
2024-09-24 18:38:25 +0300

    add links to keyboards

--- keyboards.py
+++ keyboards.py
@@ -17,16 +17,16 @@ start_kb = ReplyKeyboardMarkup(
 catalog_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text = 'Сайт 1', callback_data = 'Сайт 1'),
+            InlineKeyboardButton(text='Сайт 1', callback_data='Сайт 1', url='https://www.sostav.ru/publication/otpusk-70291.html'),
         ],
         [
-            InlineKeyboardButton(text = 'Сайт 2', callback_data = 'Сайт 2'),
+            InlineKeyboardButton(text='Сайт 2', callback_data='Сайт 2', url='https://www.sostav.ru/publication/predvzyatoe-otnoshenie-v-trudovoj-sfere-70277.html'),
         ],
         [
-            InlineKeyboardButton(text = 'Сайт 3', callback_data = 'Сайт 3'),
+            InlineKeyboardButton(text='Сайт 3', callback_data='Сайт 3', url='https://www.sostav.ru/publication/reklamnye-kompanii-trebuyut-s-distribyutorov-avto-1-mlrd-rublej-70271.html'),
         ],
         [
-            InlineKeyboardButton(text = 'Сайт 4', callback_data = 'Сайт 4'),
+            InlineKeyboardButton(text='Сайт 4', callback_data='Сайт 4', url='https://www.sostav.ru/publication/zadajte-vopros-direktoru-departamenta-marketingovykh-kommunikatsij-mts-marii-yakovlevoj-70259.html'),
         ],
     ]
 )
